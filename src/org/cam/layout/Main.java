dd9bb6a4d6073dfce7b166775906b7202849177b
Sergio <sergio@sergio.se>
2016-01-26 15:48:06 +0100

    add Set X DISPLAY configuration

--- src/org/cam/layout/Main.java
+++ src/org/cam/layout/Main.java
@@ -1,9 +1,17 @@
 package org.cam.layout;
 
+import org.cam.layout.CheckProcess;;
+
+
 public class Main {
 		public static void main(String[] args) {
 		
 			System.out.println("Sono nel main");
+			
+			//SET DEFAULT DISPLAY X SYSTEM
+			CheckProcess checkProccess = new CheckProcess();
+			checkProccess.executeCommand("export DISPLAY=:0.0");
+			
 			Finestra finestra = new Finestra();
 			finestra.mainWindow();
 			System.out.println("Ho creato la finestra");
