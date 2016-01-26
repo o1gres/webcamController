f7ca229d197909b585b10c3d86f24650ee7fb206
Sergio <sergio@sergio.se>
2016-01-26 16:31:14 +0100

    Fix

--- src/org/cam/layout/Main.java
+++ src/org/cam/layout/Main.java
@@ -1,16 +1,11 @@
 package org.cam.layout;
 
-import org.cam.layout.CheckProcess;;
 
 
 public class Main {
 		public static void main(String[] args) {
 		
 			System.out.println("Sono nel main");
-			
-			//SET DEFAULT DISPLAY X SYSTEM
-			CheckProcess checkProccess = new CheckProcess();
-			checkProccess.executeCommand("export DISPLAY=:0.0");
 			
 			Finestra finestra = new Finestra();
 			finestra.mainWindow();
